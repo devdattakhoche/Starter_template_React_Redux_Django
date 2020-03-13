07f80320f298e32043cc059517ae285e980ac807
devdattakhoche <dkhoche70@gmail.com>
2020-03-13 18:11:42 +0530

    Configuration files created

---
+++ reactdjango/webpack.config.js
@@ -0,0 +1,12 @@
+export const module = {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }
+    ]
+};
+  
